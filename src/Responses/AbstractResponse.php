936193995303ae0d2f859847516bc35d007c1b1e
Saul Chelewani <saul@patkay.net>
2020-06-20 01:52:43 +0200

    Added json alias return type

--- src/Responses/AbstractResponse.php
+++ src/Responses/AbstractResponse.php
@@ -50,6 +50,11 @@ abstract class AbstractResponse extends Response
         ], $this->code, $this->headers, JSON_PRETTY_PRINT);
     }
 
+    public function json(): JsonResponse
+    {
+        return $this->send();
+    }
+
     private function successful(): bool
     {
         return $this->code >= 200 && $this->code < 400;
